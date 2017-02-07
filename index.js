80233740af134d054ab400d74db80c4979cd8aba
Diego Corrales <diegoamcorrales@gmail.com>
2017-02-07 14:36:03 -0800

    modify oauthcallback to exchange access token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
@@ -7,13 +8,21 @@ app.use(bodyParser.json());
 
 // Lightspeed will call this endpoint
 app.post('/oauthcallback', (req, res) => {
-	console.log('-- PARAMS --');
-	console.log(JSON.stringify(req.params));
-
-	console.log('-- BODY --');
-	console.log(JSON.stringify(req.body));
-
-	res.sendStatus(200);
+	axios
+		.post('https://cloud.merchantos.com/oauth/access_token.php', {
+			client_id: process.env.LIGHTSPEED_CLIENT_ID,
+            client_secret: process.env.LIGHTSPEED_CLIENT_SECRET,
+            code: req.params.code,
+            grant_type: 'authorization_code'
+		})
+		.then((response) => {
+			console.log(JSON.stringify(response.data));
+			res.sendStatus(200);
+		})
+		.catch((error) => {
+			console.error(error);
+			res.sendStatus(500);
+		});
 });
 
 const port = process.env.PORT || 8080;
